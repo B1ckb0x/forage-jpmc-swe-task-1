fc329f757dad752bd7bab7cddb11ebf608f6ebec
Emmanuel-As <a.emmanuel0616@gmail.com>
2024-08-15 20:46:22 -0400

    feat: Add more unit tests

--- client_test.py
+++ client_test.py
@@ -8,6 +8,10 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    stock, bid_price, ask_price, price = getDataPoint(quotes[0])
+    expected_price = (120.48 + 121.2) / 2
+    self.assertEqual(price, expected_price)
+    self.assertEqual(stock, 'ABC')
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,10 +19,29 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
-
+    stock, bid_price, ask_price, price = getDataPoint(quotes[0])
+    expected_price = (120.48 + 119.2) / 2
+    self.assertEqual(price, expected_price)
+    self.assertEqual(stock, 'ABC')
   """ ------------ Add more unit tests ------------ """
 
+
+  def test_getDataPoint_multipleQuotes(self):
+    quotes = [
+      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453',
+       'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453',
+       'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    stock, bid_price, ask_price, price = getDataPoint(quotes[0])
+    expected_price = (120.48 + 121.2) / 2
+    self.assertEqual(price, expected_price)
+    self.assertEqual(stock, 'ABC')
+
+    stock, bid_price, ask_price, price = getDataPoint(quotes[1])
+    expected_price = (117.87 + 121.68) / 2
+    self.assertEqual(price, expected_price)
+    self.assertEqual(stock, 'DEF')
 
 
 if __name__ == '__main__':
